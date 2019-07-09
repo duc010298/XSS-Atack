6f45071a204010e38695d7ee8e60a13760304be8
Đỗ Trung Đức <duc010298@gmail.com>
2019-07-09 20:50:55 +0700

    Update MainController.java

--- src/main/java/com/github/duc010298/xssattack/controller/MainController.java
+++ src/main/java/com/github/duc010298/xssattack/controller/MainController.java
@@ -1,18 +1,19 @@
 package com.github.duc010298.xssattack.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/")
 public class MainController {
 
     @CrossOrigin(origins = "https://truejuice.vn")
     @PostMapping
-    public void Test(@RequestBody String object) {
+    public String Test(@RequestBody String object) {
         System.out.println(object);
+        return "Done";
     }
 }
