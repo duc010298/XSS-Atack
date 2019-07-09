f6d47afc6e361cd42fdc656a47b5404b674f7fbf
Đỗ Trung Đức <duc010298@gmail.com>
2019-07-09 20:38:17 +0700

    .

--- src/main/java/com/github/duc010298/xssattack/DemoApplication.java
+++ src/main/java/com/github/duc010298/xssattack/DemoApplication.java
@@ -2,6 +2,8 @@ package com.github.duc010298.xssattack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +11,14 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/").allowedOrigins("https://truejuice.vn");
+            }
+        };
+    }
 
 }
--- src/main/java/com/github/duc010298/xssattack/controller/MainController.java
+++ src/main/java/com/github/duc010298/xssattack/controller/MainController.java
@@ -1,14 +1,16 @@
 package com.github.duc010298.xssattack.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
+    @CrossOrigin(origins = "https://truejuice.vn")
     @PostMapping("/")
-    public void Test(@RequestParam("content") String content) {
-        System.out.println(content);
+    public void Test(@RequestParam("object") String object) {
+        System.out.println(object);
     }
 }
